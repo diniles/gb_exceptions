40e1feb4d421cd6a52f751b1ac8f4119d3d98064
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-22 15:34:06 +0300

    Feat : seminar 1 task 1

---
+++ src/seminar1/task1/Answer.java
@@ -0,0 +1,18 @@
+package seminar1.task1;
+
+public class Answer {
+    public static void arrayOutOfBoundsException() {
+        // Напишите свое решение ниже
+
+    }
+
+    public static void divisionByZero() {
+        // Напишите свое решение ниже
+
+    }
+
+    public static void numberFormatException() {
+        // Напишите свое решение ниже
+
+    }
+}
