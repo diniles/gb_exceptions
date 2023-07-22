0c6949206fae7abc549429df5e5a15c1f21f1673
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-22 15:40:06 +0300

    Feat : seminar 1 task 1 divisionByZero solved

--- src/seminar1/task1/Answer.java
+++ src/seminar1/task1/Answer.java
@@ -1,17 +1,16 @@
 package seminar1.task1;
 
-import java.lang.reflect.Array;
-
 public class Answer {
     public static void arrayOutOfBoundsException() {
         // Напишите свое решение ниже
-        int[] array=new int[]{1,2,3,4,5};
+        int[] array = new int[]{1, 2, 3, 4, 5};
         System.out.println(array[5]);
 
     }
 
     public static void divisionByZero() {
         // Напишите свое решение ниже
+        int divByZero = 1 / 0;
 
     }
 
