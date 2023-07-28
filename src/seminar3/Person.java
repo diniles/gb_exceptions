b67248572f27f55affdb49d6ad7cfd40a688b4ed
diniles <dzliashko@gmail.com>
2023-07-28 10:07:38 +0300

    Feat: seminar 3 class Person

---
+++ src/seminar3/Person.java
@@ -0,0 +1,26 @@
+package seminar3;
+
+public class Person {
+    private final String lastName;
+    private final String firstName;
+    private final String middleName;
+    private final String dateOfBirth;
+    private final String phone;
+    private final String sex;
+
+
+    public Person(String lastName, String firstName, String middleName, String dateOfBirth, String phone, String sex) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.sex = sex;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("<%s><%s><%s><%s><%s><%s>", lastName, firstName, middleName, dateOfBirth, phone, sex);
+    }
+}
