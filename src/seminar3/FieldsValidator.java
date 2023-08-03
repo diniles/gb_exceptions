6c0854671a87d020b34f684a53324883666e9077
diniles <dzliashko@gmail.com>
2023-08-03 11:02:10 +0300

    Feat: seminar 3 class FieldsValidator

---
+++ src/seminar3/FieldsValidator.java
@@ -0,0 +1,35 @@
+package seminar3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.time.DateTimeException;
+import java.time.format.DateTimeFormatter;
+
+public class FieldsValidator {
+    private static final Pattern namePattern = Pattern.compile("^[\\p{L}]+$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern phonePattern = Pattern.compile("^[0-9]{7,}$");
+
+    public static boolean illegalName(String name) {
+        Matcher matcher = namePattern.matcher(name);
+        return !matcher.matches();
+    }
+
+    public static boolean isPhoneInvalid(String phone) {
+        Matcher matcher = phonePattern.matcher(phone);
+        return !matcher.matches();
+    }
+
+    public static boolean illegalDate(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            formatter.parse(date);
+            return false;
+        } catch (DateTimeException e) {
+            return true;
+        }
+    }
+
+    public static boolean illegalSex(String sex) {
+        return !(sex.equals("m") || sex.equals("f"));
+    }
+}
--- src/seminar3/exceptions/IllegalParametersQuantityException.java
+++
@@ -1,7 +0,0 @@
-package seminar3.exceptions;
-
-public class IllegalParametersQuantityException extends RuntimeException {
-    public IllegalParametersQuantityException(String message) {
-        super("Illegal arguments quantity: " + message);
-    }
-}
