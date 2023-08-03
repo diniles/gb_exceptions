de2f30f90bc8da67de3e227afa73e470e830f862
diniles <dzliashko@gmail.com>
2023-08-03 12:24:05 +0300

    Feat: seminar 3 class finished

--- src/seminar3/Main.java
+++ src/seminar3/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter \"exit\" to exit program");
         while (true) {
