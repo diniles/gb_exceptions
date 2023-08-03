4780c07aca61d407e0947d7988c789c34556e7df
diniles <dzliashko@gmail.com>
2023-08-03 12:14:08 +0300

    Feat: seminar 3 class Main

--- src/seminar3/Main.java
+++ src/seminar3/Main.java
@@ -1,5 +1,31 @@
 package seminar3;
 
+import seminar3.exceptions.IllegalFieldException;
+
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter \"exit\" to exit program");
+        while (true) {
+            System.out.println("Enter person data like this \"Lastname Fistname Middlename DD.MM.YYY 1234567 m/f\" :");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            try {
+                Person person = Persons.createPerson(input);
+                new FileWriter(person).writeToFile();
+            } catch (IllegalFieldException e) {
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input format: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Something went wrong, please try again:" + e.getMessage());
+            }
+        }
+    }
 }
+
